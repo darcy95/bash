8c0cf265caabc9cf618b8b159113e0f9b4fe871c
Juhoon Kim <kimjuhoon@gmail.com>
2016-09-20 17:13:25 +0200

    rotate-matrix added ...

---
+++ rotate-matrix.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+export LC_ALL=C
+
+## This script rotates a matrix
+## 
+## You can use this script, for example, like this:
+##  cat matrix.log | ./rotate-matrix.sh
+##
+
+awk '{
+	if (max_nf < NF) {
+		max_nf = NF;
+	}
+
+	max_nr = NR;
+
+	for (x = 1; x <= NF; x++) {
+		vector[x, NR] = $x;
+	}
+} END {
+	for (x = 1; x <= max_nf; x++) {
+		for (y = max_nr; y >= 1; --y) {
+			printf("%s ", vector[x, y]);
+		}
+	
+		print("\n");
+	}
+}' $1
